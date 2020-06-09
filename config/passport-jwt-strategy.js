c3d02f2afe8188f0aefc30240ba38e30475c4fe0
Abhishek Jain <1709abhishek@gmail.com>
2020-06-10 01:03:14 +0530

    corrected apis

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -2,21 +2,21 @@ const passport = require('passport');
 const JWTStrategy = require('passport-jwt').Strategy;
 const ExtractJWT = require('passport-jwt').ExtractJwt;
 
-const User = require('../models/users');
+const User = require('../models/user');
 
 let opts = {
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey: 'codeial'
 }
 
-passport.use(new JWTStrategy(opts, function(jwtPayLoad, done){
+passport.use(new JWTStrategy(opts, function (jwtPayLoad, done) {
 
-    User.findById(jwtPayLoad._id, function(err, user){
-        if(err){console.log('Error in finding user from JWT'); return;}
+    User.findById(jwtPayLoad._id, function (err, user) {
+        if (err) { console.log('Error in finding user from JWT'); return; }
 
-        if(user){
+        if (user) {
             return done(null, user);
-        }else{
+        } else {
             return done(null, false);
         }
     })
